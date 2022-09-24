bcdf5fdcfcf842737172624d7b9a2b48f367c838
Andrii Sovtus <99980555+mesnuk@users.noreply.github.com>
2022-09-24 13:49:12 +0300

    Updates

---
+++ precache-manifest.d2e9e1a17e5cf6966b92663ab3e61653.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96a0f11225f40024059ccbe4802c626",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd64c52c92744cfbd9eb",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6b11f8e964d37b5443e3",
+    "url": "/static/css/main.9d147567.chunk.css"
+  },
+  {
+    "revision": "dd64c52c92744cfbd9eb",
+    "url": "/static/js/2.3a853548.chunk.js"
+  },
+  {
+    "revision": "6b11f8e964d37b5443e3",
+    "url": "/static/js/main.1a26e822.chunk.js"
+  },
+  {
+    "revision": "bae84254bf3b2e10bee2",
+    "url": "/static/js/runtime-main.3eb68076.js"
+  },
+  {
+    "revision": "7709c10e8c30753537d94a6b69d7542a",
+    "url": "/static/media/aboutBcg.7709c10e.jpeg"
+  },
+  {
+    "revision": "945afb997e0731b038ed58fadacfeb3f",
+    "url": "/static/media/contactBcg.945afb99.jpeg"
+  },
+  {
+    "revision": "0819a81634391f8518fe325378b8f994",
+    "url": "/static/media/defaultBcg.0819a816.jpeg"
+  },
+  {
+    "revision": "6f19757fb5101d8815a78371b7627756",
+    "url": "/static/media/logo.6f19757f.svg"
+  },
+  {
+    "revision": "984874d19d1cdef8dcee5fcea55c082d",
+    "url": "/static/media/mainBcg.984874d1.jpeg"
+  },
+  {
+    "revision": "160fddff3ca71887ebf8da3929244379",
+    "url": "/static/media/productsBcg.160fddff.jpeg"
+  },
+  {
+    "revision": "9675841294261b773267d557f531a8ff",
+    "url": "/static/media/singleProductBcg.96758412.jpeg"
+  },
+  {
+    "revision": "46143c2f25bc4667fac18f5924288217",
+    "url": "/static/media/storeBcg.46143c2f.jpeg"
+  }
+]);
